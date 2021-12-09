769e66f503bc6262b677823a8c259aa7e5d614a0
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-09 12:42:57 +0530

    __init__.py

---
+++ main/__init__.py
@@ -0,0 +1,16 @@
+#ChauhanMahesh/Vasusen/DroneBots/COL
+
+from telethon import TelegramClient
+from decouple import config
+import logging
+import time
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# Basics
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
