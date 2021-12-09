1d4de2d1f7007cd9db64ec40501dc5626de7feb3
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-09 12:45:18 +0530

    Create __main__.py

---
+++ main/__main__.py
@@ -0,0 +1,21 @@
+import glob
+from pathlib import Path
+from main.utils import load_plugins
+import logging
+from . import bot
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+path = "main/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("Successfully deployed!")
+
+if __name__ == "__main__":
+    bot.run_until_disconnected()
