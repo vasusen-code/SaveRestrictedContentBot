8681cbe64748cace23473f5b5ed4ff8afcdc1c0c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 00:02:20 +0530

    Update get_msg.py

--- main/plugins/get_msg.py
+++ main/plugins/get_msg.py
@@ -4,7 +4,7 @@
 from telethon import errors
 from telethon.tl.functions.messages import ImportChatInviteRequest
 
-async def join(client, invite_link)
+async def join(client, invite_link):
     try:
         await client(ImportChatInviteRequest(invite_link))
         return "Successfully joined the Channel"
