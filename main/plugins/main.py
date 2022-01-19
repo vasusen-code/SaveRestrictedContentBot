412c4711642458b950871271d87f1c6a59b57a25
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-19 23:34:24 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -1,10 +1,23 @@
+import re
 from telethon import events
 
+from .. import Drone
+from main.plugins.get_msg import get_msg
+
+def get_link(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)   
+    try:
+        link = [x[0] for x in url][0]
+        if link:
+            return link
+        else:
+            return False
+    except Exception:
+        return False
+    
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def m(event):
-    yy = await force_sub(event.sender_id)
-    if yy is True:
-        return await event.reply(forcesubtext)
     link = get_link(event.text)
     if not link:
         return
@@ -15,6 +28,4 @@ async def m(event):
             msg = await get_msg(chat_id, msg_id)
         except Exception as e:
             await event.reply(f'Error: `{str(e)}`')
-        if msg.video:
-            
-        else:
+        await event.client.send_message(event.chat_id, msg) 
