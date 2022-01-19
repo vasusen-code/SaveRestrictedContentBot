5b9591d583199c865af1545593eb739703ae0684
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-19 16:51:36 +0530

    Update and rename empty to get_msg.py

---
+++ main/plugins/get_msg.py
@@ -0,0 +1,22 @@
+# ChauhanMahesh/Vasusen/COL/DroneBots
+# Github.com/Vasusen-code
+
+from telethon import errors
+from telethon.tl.functions.messages import ImportChatInviteRequest
+
+async def join(client, invite_link)
+    try:
+        await client(ImportChatInviteRequest(invite_link))
+        return "Successfully joined the Channel"
+    except errors.UserAlreadyParticipantError:
+        return "You have already joined the Channel"
+    except errors.InviteHashExpiredError:
+        return "Wrong URL"
+      
+async def get_msg(client, msg_link):
+    chat = int(str(-100) + str(msg_link.split("/")[-2]))
+    msg_id = int(msg_link.split("/")[-1])
+    msg = await client.get_messages(chat, offset_id=msg_id)
+    if msg.video:
+    
+    
