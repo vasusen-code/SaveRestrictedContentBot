e825e20d2b38b19719ab267016c95795de28b3b5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-19 23:40:12 +0530

    Added UB

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,9 @@
 #ChauhanMahesh/Vasusen/DroneBots/COL
 
-from telethon import TelegramClient
+import time, logging
 from decouple import config
-import logging
-import time
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -12,5 +12,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+client = TelegramClient(StringSession(SESSION) , APP_ID, API_HASH)
+try:
+    client.start()
+except BaseException:
+    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
+
+Drone = TelegramClient('Drone', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
