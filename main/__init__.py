3f4387822704182f8aaf747d03bd130d5ec5c447
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 14:35:49 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,14 +15,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-
-client = TelegramClient(StringSession(SESSION) , API_ID, API_HASH)
-try:
-    client.start()
-except BaseException:
-    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
-    sys.exit(1)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
