2897d8cc693b0f871ac14138c8bf765a426ac237
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 20:05:40 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -10,3 +10,4 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
