dc84b2e909a4004ec0db46f99fb870baa00f7517
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 15:01:34 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -1,11 +1,13 @@
 import re
 from telethon import events
 
-from .. import robot
+from .. import API_ID, API_HASH, BOT_TOKEN
 from .. import bot as Drone
 
 from pyrogram import client, filters
 from main.plugins.get_msg import get_msg
+
+robot = Client('robot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 def get_link(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
@@ -21,18 +23,17 @@ def get_link(string):
     
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def m(event):
-    q = []
-    link = get_link(event.text)
+    pass
+  
+@robot.on_message(filters.private)
+async def run(client, message):
+    link = get_link(message.text)
     if not link:
         return
     if 't.me' in link:
         try:
             await get_msg(robot, event.sender_id, link)
         except Exception as e:
-            await event.reply(f'Error: `{str(e)}`')
-  
-@robot.on_message(filters.private)
-async def run(client, message):
-    pass
+            return await message.reply(f'Error: `{str(e)}`')
 
 robot.run()
