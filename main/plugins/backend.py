80c7792b6ced8977efc746b6ea6d635952cd3761
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 18:55:12 +0530

    Update backend.py

--- main/plugins/backend.py
+++ main/plugins/backend.py
@@ -1,5 +1,7 @@
 #Github.com/Vasusen-code
-from .. import API_ID, BOT_TOKEN, API_HASH
+
+from main.plugins.helpers import get_link
+from .. import API_ID, BOT_TOKEN, API_HASH, SESSION
 from pyrogram import Client, filters
 import re
 
@@ -10,33 +12,33 @@ Bot = Client(
     api_hash=API_HASH
 )
 
-async def get_msg(client, sender, msg_link):
+userbot = Client(
+    session_name=SESSION, 
+    api_hash=API_HASH, 
+    api_id=API_ID
+)
+
+async def get_msg(userbot, client, sender, msg_link):
     chat = msg_link.split("/")[-2]
     msg_id = int(msg_link.split("/")[-1])
-    await client.copy_message(sender, chat, msg_id)
-    
-    
-def get_link(string):
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)   
-    try:
-        link = [x[0] for x in url][0]
-        if link:
-            return link
-        else:
-            return False
-    except Exception:
-        return False
+    if 't.me/c' in msg_link:
+        msg = await userbot.copy_message("me", chat, msg_id)
+        await client.send_message(int(sender), msg) 
+    else:
+        await client.copy_message(int(sender), chat, msg_id)
     
 @Bot.on_message(filters.private)
 async def start(bot, event):
     link = get_link(event.text)
     if not link:
         return
+    if 't.me/+' in link:
+        return
     if 't.me' in link:
         try:
-            await get_msg(bot, event.chat.id, link)
+            await get_msg(userbot, bot, event.chat.id, link)
         except Exception as e:
             return await event.reply(f'Error: `{str(e)}`')
 
 Bot.run()
+userbot.run()
