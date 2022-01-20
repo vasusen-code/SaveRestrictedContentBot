fc8cb57779714d9e22fa24d58d5d8edadf594a21
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 15:38:09 +0530

    Update get_msg.py

--- main/plugins/get_msg.py
+++ main/plugins/get_msg.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 async def get_msg(client, sender, msg_link):
     chat = msg_link.split("/")[-2]
     msg_id = int(msg_link.split("/")[-1])
-    msg = await client.get_messages(chat, msg_id)
-    await client.send_message(sender, msg.copy)
+    msg = await client.get_messages(chat, ids=msg_id)
+    await client.send_message(sender, msg)
     
     
