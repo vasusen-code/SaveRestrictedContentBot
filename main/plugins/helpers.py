cb90b255c2f5d6ee7d6dcc4980b7ffffea56f90d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-20 19:38:13 +0530

    Update helpers.py

--- main/plugins/helpers.py
+++ main/plugins/helpers.py
@@ -1,19 +1,24 @@
 from telethon import errors
-from telethon.tl.functions.messages import ImportChatInviteRequest
-
+from pyrogram.raw.functions.messages import ImportChatInvite
 from decouple import config
 
 forcesub_text = 'You have to join @Dronebots to use this bot.'
 
+#Join private chat-------------------------------------------------------------------------------------------------------------
+
 async def join(client, invite_link):
     try:
-        await client(ImportChatInviteRequest(invite_link))
+        await client(ImportChatInvite(invite_link))
         return "Successfully joined the Channel"
-    except errors.UserAlreadyParticipantError:
-        return "Already joined the Channel"
-    except errors.InviteHashExpiredError:
-        return "Wrong URL"
-    
+    except Exception as e:
+        print(e)
+        if 'already' or 'Already' in str(e):
+            return "Already joined."
+        else:
+            return "Could not join. Maybe your link is expired."
+        
+#forcesub-------------------------------------------------------------------------------------------------------------
+
 async def forcesub(id):
     FORCESUB = config("FORCESUB", default=None)
     if FORCESUB is not None:
@@ -30,3 +35,18 @@ async def forcesub(id):
     except UserNotParticipantError:
         ok = True 
     return ok
+
+#Regex---------------------------------------------------------------------------------------------------------------
+#to get the url from event
+
+def get_link(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)   
+    try:
+        link = [x[0] for x in url][0]
+        if link:
+            return link
+        else:
+            return False
+    except Exception:
+        return False
