e538b5019fdc7a4877385668a6c8c4c9a591c3d1
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-23 02:11:52 +0530

    Update display_progress.py

--- main/plugins/display_progress.py
+++ main/plugins/display_progress.py
@@ -34,12 +34,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "**[{0}{1}] | {2}%**\n\n".format(
+        progress = "**[{0}{1}]** `| {2}%`\n\n".format(
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
+        tmp = progress + "GROSSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
