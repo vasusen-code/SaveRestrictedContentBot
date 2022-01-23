2a0c924d2174ca050e022d72f8015fa68b59923a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-23 10:12:19 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -103,13 +103,6 @@ async def clone(bot, event):
     if xx is True:
         await event.reply_text(text=forcesub_text)
         return
-    try:
-        await start_userbot(userbot)
-    except Exception as e:
-        if 'Client is already connected' in str(e):
-            pass
-        else:
-            return
     if 't.me/+' in link:
         xy = await join(userbot, link)
         await event.reply_text(text=xy)
@@ -124,3 +117,4 @@ async def clone(bot, event):
                 return await event.reply_text(text=f'Error: `{str(e)}`')
 
 Bot.run()
+asyncio.run(start(start_userbot(userbot)))
