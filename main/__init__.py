302ba262f746c16de7107b173dd1078048ca31a9
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-23 15:51:56 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,11 +1,19 @@
 #ChauhanMahesh/Vasusen/DroneBots/COL
 
+from telethon import TelegramClient
 from decouple import config
+import logging
+import time
 
-# Basics
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# variables
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None) #pyro session
-FORCESUB = config("FORCESUB", default=None) 
-ACCESS = config("ACCESS", default=None, cast=int)
+FORCESUB = config("FORCESUB", default=None, cast=int)
+ACCESS = config("ACCESS_CHANNEL", default=None)
+SESSION = config("SESSION", default=None)
+
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
