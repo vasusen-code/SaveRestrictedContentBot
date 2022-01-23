23cd6206713845b51c197b67570e27fee25cd3f4
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-23 14:46:26 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,14 +1,11 @@
 #ChauhanMahesh/Vasusen/DroneBots/COL
 
-import sys, time, logging
 from decouple import config
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
 
 # Basics
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
+SESSION = config("SESSION", default=None) #pyro session
+FORCESUB = config("FORCESUB", default=None) 
 ACCESS = config("ACCESS", default=None, cast=int)
