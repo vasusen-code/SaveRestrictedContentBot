b20d8e9fa3e8f7164c916efe4cc0501607063b33
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-23 14:45:08 +0530

    Create start.py

---
+++ main/plugins/start.py
@@ -0,0 +1,52 @@
+from .. import API_ID, API_HASH, BOT_TOKEN
+
+from telethon import events, Button, TelegramClient
+
+#connection
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+bot.run_until_disconnected()
+
+st = 
+
+@bot.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply(f'{st}', 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="sett"),
+                              [Button.inline("REM THUMB.", data="remt")]
+                              ])
+                        
+@bot.on(events.callbackquery.CallbackQuery(data="sett"))
+async def sett(event):    
+    Drone = event.client                    
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        x = await conv.get_reply()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found.")
+        await xx.delete()
+        t = await event.client.send_message(event.chat_id, 'Trying.')
+        path = await event.client.download_media(x.media)
+        if os.path.exists(f'{event.sender_id}.jpg'):
+            os.remove(f'{event.sender_id}.jpg')
+        os.rename(path, f'./{event.sender_id}.jpg')
+        await t.edit("Temporary thumbnail saved!")
+        
+@bot.on(events.callbackquery.CallbackQuery(data="remt"))
+async def remt(event):  
+    Drone = event.client            
+    await event.edit('Trying.')
+    try:
+        os.remove(f'{event.sender_id}.jpg')
+        await event.edit('Removed!')
+    except Exception:
+        await event.edit("No thumbnail saved.")                        
+    
