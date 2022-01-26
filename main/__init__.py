535d2141f54b89ae2c2768d883a5678602a7ef08
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-26 10:10:23 +0530

    rm Access and Fb

--- main/__init__.py
+++ main/__init__.py
@@ -12,8 +12,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-FORCESUB = config("FORCESUB", default=None, cast=int)
-ACCESS = config("ACCESS", default=None, cast=int)
 SESSION = config("SESSION", default=None)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
