ec90d3e9270af774c0efa252aa305e654aac9dfe
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 20:32:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,6 +14,7 @@ API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
