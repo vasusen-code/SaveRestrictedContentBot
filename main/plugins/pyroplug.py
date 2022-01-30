9ac6ded67a4a188579c07cce0b4245b1f36e2ebe
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 19:12:26 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -13,11 +13,20 @@ Bot = Client(
     api_hash=API_HASH
 )
 
-async def copy_message(client, sender, msg_link):
-    chat =  msg_link.split("/")[-2]
-    msg_id = msg_link.split("/")[-1]
-    await client.copy_message(int(sender), chat, msg_id)
-
-@Bot.on_message(filters.private & filters.incoming)
+@Bot.on_message(filters.private & filters.outgoing)
 async def _(bot, event):
-    pass
+    if (str(event.text)).lower().startswith("cloning"):
+        c = (event.text).split(" ")[1]
+        try:
+            chat = c.split("-")[0]     
+            msg_id = int(c.split("-")[1])
+            await Bot.copy_message(event.chat.id, chat, msg_id)
+            await event.delete()
+        except ValueError:
+            await event.edit("Send me only message link or Invite of private channel.")
+        except Exception as e:
+            if 'username' in str(e):
+                await event.edit("Couldn't clone message, maybe i am banned from the given chat.")
+            else:
+                await event.edit(str(e))
+            
