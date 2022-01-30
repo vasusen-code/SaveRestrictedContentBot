38d65deaded8f1d76cc94146bbd6deab97518297
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 22:15:14 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -3,8 +3,7 @@
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 from decouple import config
-import logging
-import time
+import logging, time, sys
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
