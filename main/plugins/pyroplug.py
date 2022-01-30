186b1432902d76d21598c116aec2f69e50015b29
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 14:02:14 +0530

    loop

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -15,3 +15,10 @@ async def copy_message(client, sender, msg_link):
     chat =  msg_link.split("/")[-2]
     msg_id = msg_link.split("/")[-1]
     await client.copy_message(int(sender), chat, msg_id)
+
+async def run_bot():
+    await Bot.start()
+    await idle()
+
+if __name__ == "__main__":
+    Bot.loop.run_until_complete(run_bot())
