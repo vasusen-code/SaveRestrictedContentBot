3255c0eb60aa621d6ba491433ad732bd37a6779e
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 18:54:53 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -18,3 +18,6 @@ async def copy_message(client, sender, msg_link):
     msg_id = msg_link.split("/")[-1]
     await client.copy_message(int(sender), chat, msg_id)
 
+@Bot.on_message(filters.private & filters.incoming)
+async def _(bot, event):
+    pass
