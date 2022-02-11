c976be3161a4a7d618e7553a2f8f1c5167e9420a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-11 19:59:41 +0530

    Update _start.py

--- main/plugins/_start.py
+++ main/plugins/_start.py
@@ -44,5 +44,6 @@ async def remt(event):
   
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
-    await start_srb(event)
+    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TeamDrone"
+    await start_srb(event, text)
     
