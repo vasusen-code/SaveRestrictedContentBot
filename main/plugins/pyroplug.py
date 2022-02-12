ccf9f3414f7356a69aaa863ffe8a84414e0ee5c7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-12 10:22:05 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -9,23 +9,6 @@ from main.plugins.helpers import screenshot
 from pyrogram import Client, filters 
 from ethon.pyfunc import video_metadata
 
-@Bot.on_message(filters.private & filters.outgoing)
-async def _(bot, event):
-    if (str(event.text)).lower().startswith("cloning"):
-        c = (event.text).split(" ")[1]
-        try:
-            chat = c.split("-")[0]     
-            msg_id = int(c.split("-")[1])
-            await Bot.copy_message(event.chat.id, chat, msg_id)
-            await event.delete()
-        except ValueError:
-            await event.edit("Send me only message link or Invite of private channel.")
-        except Exception as e:
-            if 'username' in str(e):
-                await event.edit("Couldn't clone message, maybe i am banned from the given chat.")
-            else:
-                await event.edit(str(e))
-            
 def thumbnail(sender):
     if os.path.exists(f'{sender}.jpg'):
         return f'{sender}.jpg'
