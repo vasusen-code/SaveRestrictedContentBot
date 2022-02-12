9e81b7805c00476e75759f2092cf156e6a8daf47
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-13 02:15:19 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -104,3 +104,90 @@ async def get_msg(userbot, client, sender, edit_id, msg_link):
             print(e)
         await edit.delete()
         
+async def get_msg(userbot, client, sender, msg_link, _range):
+    edit = await client.send_message(sender, 'Processing!')
+    edit_id = edit.message_id
+    chat = ""
+    msg_id = int(msg_link.split("/")[-1] + _range)
+    if 't.me/c/' in msg_link:
+        chat = int('-100' + str(msg_link.split("/")[-2]))
+        try:
+            msg = await userbot.get_messages(chat, msg_id)
+            if msg.media:
+                if 'web_page' in msg.media:
+                    edit = await client.edit_message_text(sender, edit_id, "Cloning.")
+                    await client.send_message(sender, msg.text.markdown)
+                    await edit.delete()
+                    return
+            if not msg.media:
+                if msg.text:
+                    edit = await client.edit_message_text(sender, edit_id, "Cloning.")
+                    await client.send_message(sender, msg.text.markdown)
+                    await edit.delete()
+                    return
+            edit = await client.edit_message_text(sender, edit_id, "Trying to Download.")
+            file = await userbot.download_media(
+                msg,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    client,
+                    "**DOWNLOADING:**\n",
+                    edit,
+                    time.time()
+                )
+            )
+            await edit.edit('Prearing to Upload!')
+            caption = str(file)
+            if msg.caption is not None:
+                caption = msg.caption
+            if str(file).split(".")[-1] == 'mkv' or 'mp4' or 'webm':
+                if str(file).split(".")[-1] == 'webm' or 'mkv':
+                    path = str(file).split(".")[0] + ".mp4"
+                    os.rename(file, path) 
+                    file = str(file).split(".")[0] + ".mp4"
+                data = video_metadata(file)
+                duration = data["duration"]
+                thumb_path = await screenshot(file, duration/2, sender)
+                await client.send_video(
+                    chat_id=sender,
+                    video=file,
+                    caption=caption,
+                    supports_streaming=True,
+                    duration=duration,
+                    thumb=thumb_path,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        client,
+                        '**UPLOADING:**\n',
+                        edit,
+                        time.time()
+                    )
+                )
+            else:
+                thumb_path=thumbnail(sender)
+                await client.send_document(
+                    sender,
+                    file, 
+                    caption=caption,
+                    thumb=thumb_path,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        client,
+                        '**UPLOADING:**\n',
+                        edit,
+                        time.time()
+                    )
+                )
+            await edit.delete()
+        except Exception:
+            await client.send_message(sender, f"Couldn't get this message: `t.me/c/{Chat}/{msg_id}`")
+            return 
+    else:
+        edit = await client.edit_message_text(sender, edit_id, "Cloning.")
+        chat =  msg_link.split("/")[-2]
+        try:
+            await client.copy_message(int(sender), chat, msg_id)
+        except Exception:
+            return await client.send_message(sender, f"Couldn't get this message: `t.me/{Chat}/{msg_id}`")
+        await edit.delete()
+        
