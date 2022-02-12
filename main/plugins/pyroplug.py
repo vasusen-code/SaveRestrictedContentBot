2870ea3fbce8a1f021573e33ae2641e0a4d567f8
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-12 08:44:11 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -23,3 +23,77 @@ async def _(bot, event):
             else:
                 await event.edit(str(e))
             
+def thumbnail(sender):
+    if os.path.exists(f'{sender}.jpg'):
+        return f'{sender}.jpg'
+    else:
+         return None
+      
+async def get_msg(userbot, client, sender, msg_link, edit):
+    chat = ""
+    msg_id = int(msg_link.split("/")[-1])
+    if 't.me/c/' in msg_link:
+        chat = int('-100' + str(msg_link.split("/")[-2]))
+        try:
+            msg = await userbot.get_messages(chat, msg_id)
+            file = await userbot.download_media(
+                msg,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    userbot,
+                    "**DOWNLOADING:**\n",
+                    edit,
+                    time.time()
+                )
+            )
+            await edit.edit('Trying to Upload.')
+            caption = ""
+            if msg.text is not None:
+                caption = msg.text
+            if str(file).split(".")[-1] == 'mkv' or 'mp4' or 'webm':
+                if str(file).split(".")[-1] == 'webm' or 'mkv':
+                    path = str(file).split(".")[0] + ".mp4"
+                    os.rename(file, path) 
+                    file = str(file).split(".")[0] + ".mp4"
+                data = video_metadata(file)
+                duration = data["duration"]
+                thumb_path = await screenshot(file, duration/2, sender)
+                await client.send_video(
+                    chat_id=sender,
+                    video=file,
+                    caption=caption,
+                    supports_streaming=True,
+                    duration=duration,
+                    thumb=thumb_path,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        client,
+                        '**UPLOADING:**\n',
+                        edit,
+                        time.time()
+                    )
+                )
+            else:
+                thumb_path=thumbnail(sender)
+                await client.send_document(
+                    sender,
+                    file, 
+                    caption=caption,
+                    thumb=thumb_path,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        client,
+                        '**UPLOADING:**\n',
+                        edit,
+                        time.time()
+                    )
+                )
+            await edit.delete()
+        except Exception as e:
+            await edit.edit(f'ERROR: {str(e)}')
+            return 
+    else:
+        chat =  msg_link.split("/")[-2]
+        await client.copy_message(int(sender), chat, msg_id)
+        await edit.delete()
+        
