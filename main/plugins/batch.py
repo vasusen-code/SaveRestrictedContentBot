b249d154d2efeab158e32bb3ee3d6c27ee78ac01
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-13 09:49:01 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -66,7 +66,7 @@ async def _batch(event):
                     return await conv.send_message("You can only get upto 100 files in a single batch.")
             except ValueError:
                 return await conv.send_message("Range must be an integer!")
-            s, r = await check(userbot, client, _link)
+            s, r = await check(userbot, Bot, _link)
             if s != True:
                 await conv.send_message(r)
                 return
