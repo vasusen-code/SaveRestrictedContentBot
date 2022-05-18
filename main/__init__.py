6e87f23e4b359bdce83ff9f92448a88b8a15c2df
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-05-18 08:13:11 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -22,7 +22,7 @@ AUTH = config("AUTH", default=None, cast=int)
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 userbot = Client("saverestricted",
-    session_string=SESSION, 
+    session_name=SESSION, 
     api_hash=API_HASH, 
     api_id=API_ID)
 
