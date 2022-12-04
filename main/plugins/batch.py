d41a3d9658e587684b930eda00c292258df49255
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-12-04 22:25:39 +0530

    fw.x

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -93,7 +93,7 @@ async def run_batch(userbot, client, sender, link, _range):
         try:
             await get_bulk_msg(userbot, client, sender, link, i) 
         except FloodWait as fw:
-            await asyncio.sleep(fw.seconds + 5)
+            await asyncio.sleep(fw.x + 5)
             await get_bulk_msg(userbot, client, sender, link, i)
         protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
         time.sleep(timer)
