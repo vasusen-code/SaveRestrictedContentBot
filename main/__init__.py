e84422f67195f94b469a7dadccea094420702081
Captionbro14 <116630398+Captionbro14@users.noreply.github.com>
2022-12-09 11:31:22 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("API_ID", ="6722919", cast=int)
+API_HASH = config("API_HASH", ="82d6d7b498bfba5ea9959deff02c8a4b")
+BOT_TOKEN = config("BOT_TOKEN", ="5314869516:AAFcUf7Ga492e9PA88HIpspOs2RA0iiCGfA")
+SESSION = config("SESSION", ="BQCJB5JBtRgD5EA54X4S3fYx8iLVOzQzflDBg_AWquCvwu3R4CWGGnz200NP7LJBHGV0uszePajc5GhOw1qhzqxT1lkBoHXedhagYK-JpXPXpXTZfRR0413xhpZfblJErm9FTi5cRpsxGEHigwFZlG_i9Wiw5cSaNlNfEW9gCRRgQjwDhtzJCm2K8AwH7q3RFjWnEMQZOKAyg0YdmtG1Brq1y2jGuB-DqOKUS6lJB5PVN5eEIr16I4AgnBGmDFVFu1imE3HX-1V4srcA9PsXAEzsSOmqdCunVEy9hWkKH4ImqzCy2oUOQ34ZHyuqyh75vWYvmU1r1pD49yBkahMzw7jpU1gxogA")
+FORCESUB = config("FORCESUB", ="PrimeMovieCity")
+AUTH = config("AUTH", ="1398288802", cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
