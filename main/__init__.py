c41653cd4728ce522bc57b66c4a07d548bfaa5df
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-05-28 16:11:33 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -21,10 +21,7 @@ AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client(
-    session_name=SESSION, 
-    api_hash=API_HASH, 
-    api_id=API_ID)
+userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
