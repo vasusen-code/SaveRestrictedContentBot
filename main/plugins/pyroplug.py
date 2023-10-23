d82f9163b4a5e8ae2cd5d1bcb2458cf03e91e81d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-10-23 16:34:16 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -201,11 +201,12 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
         edit = await client.edit_message_text(sender, edit_id, "Cloning.")
         chat =  msg_link.split("t.me")[1].split("/")[1]
         try:
-            msg = await client.copy_message(sender, chat, msg_id)
+            msg = await client.get_messages(chat, msg_id)
             if msg.empty:
                 new_link = f't.me/b/{chat}/{int(msg_id)}'
                 #recurrsion 
                 return await get_msg(userbot, client, bot, sender, edit_id, new_link, i)
+        await client.copy_message(sender, chat, msg_id)
         except Exception as e:
             print(e)
             return await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`\n\nError: {str(e)}')
